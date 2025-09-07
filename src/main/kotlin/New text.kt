caa8ac3de0c00439d040f795bc1b307a3dbcd9fb
p.shaporova <wapidoo@yandex.ru>
2025-09-07 23:41:37 +0300

    New file with a text

---
+++ src/main/kotlin/New text.kt
@@ -0,0 +1,8 @@
+package org.example
+
+fun main() {
+    val name = "Kotik"
+    //TIP Press <shortcut actionId="ShowIntentionActions"/> with your caret at the highlighted text
+    // to see how IntelliJ IDEA suggests fixing it.
+    println("Howdy, " + name + "!")
+}
