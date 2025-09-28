5880df3c5f9bc51bd4f0751dfcc4a82ba993c300
p.shaporova <wapidoo@yandex.ru>
2025-09-29 00:10:33 +0300

    cycles homework

---
+++ src/main/kotlin/org/example/lessons/lesson07/homework/Cycle.kt
@@ -0,0 +1,79 @@
+package org.example.org.example.lessons.lesson07.homework
+
+fun main() {
+//    for (i in 1..5) {
+//        println(i)
+//    }
+
+
+//    for (i in 2..10 step 2) {
+//        println(i)
+//    }
+
+//    for (i in 5 downTo 1) {
+//        println(i)
+//    }
+
+//    for (i in 10 downTo 1 step 2) {
+//        println(i)
+//    }
+
+//    for (i in 1..9 step 2) {
+//        println(i)
+//    }
+
+//    for (i in 1..20 step 3) {
+//        println(i)
+//    }
+
+//    val size: Int = 43
+//    for (i in 3..< size step 2) {
+//        println(i)
+//    }
+
+//    var number: Int = 1
+//    while(number < 6) {
+//        println(number*number)
+//        number++
+//    }
+
+//    var number: Int = 10
+//    while(number > 5) {
+//        println(number)
+//        number--
+//    }
+
+//    var number: Int = 5
+//    do {
+//        println(number)
+//    } while (--number >= 1 )
+
+//    var number: Int = 5
+//    do {
+//        println(number)
+//    } while (++number < 10)
+
+//    for (i in 1..10) {
+//        if (i == 6) break
+//        println(i)
+//    }
+
+//    var number: Int = 1
+//    while(number >= 1) {
+//        if (number == 10) break
+//        println(number)
+//        number++
+//    }
+
+//    for (i in 1..10) {
+//        if (i % 2 == 0) continue
+//        println(i)
+//    }
+
+    for (i in 1..10) {
+        if (i % 3 == 0) continue
+        println(i)
+    }
+
+
+}
